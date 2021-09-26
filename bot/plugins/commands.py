e9ce3f66879e5e8a56dace37f1ca5fc3120b84b1
Xrsirx5 <91307602+Xrsirx5@users.noreply.github.com>
2021-09-26 09:07:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = @nougthylant,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '⭕our channel⭕', url=https://t.me/noughtyland1
                                 )
                         ]
                     ]
